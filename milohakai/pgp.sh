2158690ea05b1be6827bb79030e1ef97ef86c010
MiloHakai <81438550+MiloHakai@users.noreply.github.com>
2026-01-06 16:28:32 +0100

    added pgp

---
+++ milohakai/pgp.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+cat << 'EOF'
+[0;37m┌───────────────────────────────────────────────────────────────────────────────────────────┐
+│[1;34m⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀[0;37m [1;37mMiloHakai[0;37m                            
+│[1;34m⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀[0;37m ──────────────────────────────────────────────────────
+│[1;34m⠀⠀⠀⠀⠀⠀⠀⠀⢀⣀⣠⣬⣤⣤⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀[0;37m [1;34mAlgorithm:[0;37m ED25519                 
+│[1;34m⠀⠀⠀⠀⠀⢠⣶⠟⣿⣿⣿⣿⣿⣿⣿⣿⣷⣤⡀⠀⠀⠀⠀⠀[0;37m [1;34mEncryption:[0;37m CURVE25519            
+│[1;34m⠀⠀⠀⠀⢠⣿⠏⣼⣿⣿⠿⣿⣿⣿⣿⣿⣿⣿⣿⣦⡀⠀⠀⠀[0;37m [1;34mCreated:[0;37m 2026-01-06               
+│[1;34m⠀⠀⠀⠀⣾⣿⡆⣿⣿⡃⣿⣧⣝⢿⡻⣿⣿⣿⣿⣿⣷⠀⠀⠀[0;37m                                    
+│[1;34m⠀⠀⠀⢸⣿⣿⣿⢸⣿⣴⣾⣿⣿⣯⣳⣜⢿⣿⣿⣿⣿⡇⠀⠀[0;37m [1;34mFingerprint:[0;37m                   
+│[1;34m⠀⠀⠀⡌⣿⣿⣿⡈⣿⢀⣤⣤⣭⡉⠙⢿⡏⣿⣿⣿⣿⡇⠀⠀[0;37m 2831 27F6 ECE9 740F               
+│[1;34m⠀⠀⢀⢸⡟⣿⣿⢷⡌⢸⣿⣿⣿⣿⣷⡜⢷⣿⣿⣿⣿⡇⠀⠀[0;37m 144C 0CF4 2B95 F432               
+│[1;34m⠀⠀⠸⢘⡀⢻⣿⣦⡀⣙⡛⠛⠛⠿⠿⠿⢸⣿⣿⣿⣿⠃⠀⠀[0;37m 79B2 8F7A                         
+│[1;34m⠀⠀⠀⢰⠀⢠⣍⠙⠻⣿⣿⣿⣷⣶⣦⠀⣾⣿⣿⣿⠃⠀⠀⠀[0;37m                                    
+│[1;34m⠀⠀⠀⠈⢦⣾⢹⣿⣦⢘⣋⡁⠀⠀⢙⠃⣿⣿⠟⠁⠀⠀⠀⠀[0;37m [1;34mSubkeys:[0;37m                        
+│[1;34m⠀⠀⠀⠀⠈⢻⣾⣿⡏⠸⣿⡿⠿⠿⠟⢀⣡⣄⠀⠀⠀⠀⠀⠀[0;37m └─ [E] 4D48 4B12 78D5 6141        
+│[1;34m⠀⠀⠀⠀⠀⠈⣾⠟⠀⠀⠠⢤⣼⣿⣆⢻⣿⣿⣷⡀⠀⠀⠀⠀[0;37m                                   
+│[1;34m⠀⠀⠀⠀⠀⠘⠁⣊⣐⡀⣴⣶⡼⠟⣡⣾⣿⣿⣿⣷⡀⠀⠀⠀[0;37m [1;31mWarning:[0;37m gpg.fail                  
+│[1;34m⠀⠀⠀⡀⠀⢀⣴⣿⣿⣿⠿⠋⠀⠘⣻⣿⡿⢻⣿⣿⣿⢆⠀⠀[0;37m                                    
+│[1;34m⠀⠀⣠⡅⠐⢿⣙⠾⠿⠁⣰⣶⡾⢘⣋⣩⣾⡆⣿⣿⣿⣿⡆⠀[0;37m [1;34mImport:[0;37m                           
+│[1;34m⠀⠀⣿⡇⠀⠀⠙⠷⢷⣶⣭⣍⡁⣾⣿⣿⣿⡗⣿⣿⣿⣿⠇⠀[0;37m curl -s outoftune.dev/milohakai/pgp | gpg --import
+│[1;34m⠀⠀⢿⡇⠀⠀⠀⠀⣿⣶⣶⡄⢿⡛⠛⠛⠛⣸⠿⢏⠉⠀⠀⠀[0;37m                                    
+│[1;34m⠀⠀⠀⠃⠀⠀⠀⠀⠙⠛⠛⠁⠀⠈⠉⠀⠁⠀⠀⠀⠀⠀⠀⠀[0;37m                                   
+└───────────────────────────────────────────────────────────────────────────────────────────────[0m
+EOF
+
+# Output the actual PGP key
+cat << 'PGPKEY'
+-----BEGIN PGP PUBLIC KEY BLOCK-----
+mDMEaV0cUxYJKwYBBAHaRw8BAQdAtre/fcOIA8r4SskRbs7SpE9vqimOwACyRKyG
+VNu0+xe0TG1pbG9oYWthaSAoTXkgb3duIEdQRyBrZXkgOjMsIGNoZWNrIG91dCBn
+cGcuZmFpbCkgPG1pbG9oYWthMUBwcm90b25tYWlsLmNvbT6IrwQTFgoAVxYhBCgx
+J/bs6XQPFEwM9CuV9DJ5so96BQJpXRxTGxSAAAAAAAQADm1hbnUyLDIuNSsxLjEx
+LDIsMQIbAwULCQgHAgIiAgYVCgkICwIEFgIDAQIeBwIXgAAKCRArlfQyebKPerr+
+AP9DjWLosmdQdyB+gQu1NFQVUAtOgMYIbSFYH4PLEuLzGwD/dNkltIEuZuZImPOD
+FST5JdzePXSMBn5mixyxFOUyXwe4OARpXRxTEgorBgEEAZdVAQUBAQdAgWyhrdPG
+LEjpLC+uCX4dy4XERp8IcU+pqnAybHjfBkADAQgHiJQEGBYKADwWIQQoMSf27Ol0
+DxRMDPQrlfQyebKPegUCaV0cUxsUgAAAAAAEAA5tYW51MiwyLjUrMS4xMSwyLDEC
+GwwACgkQK5X0Mnmyj3pydQEAni6ItQ577qcPRCzBFhpJY5a4ppm37bH6ZGi9U0n1
+J5EA/0mTOwivgfQ2rXVyogEI3bYE/bkvX17N6FAZaqV8UK4G
+=jiMj
+-----END PGP PUBLIC KEY BLOCK-----
+PGPKEY
