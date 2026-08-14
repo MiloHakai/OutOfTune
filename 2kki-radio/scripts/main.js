03cf2845595c5be27697acab7bff59f8b92d9e85
MiloHakai <MiloHaka1@protonmail.com>
2026-08-14 17:25:40 +0200

    Update main.js

--- 2kki-radio/scripts/main.js
+++ 2kki-radio/scripts/main.js
@@ -1,8 +1,6 @@
-// --- CONFIGURAZIONE ---
 const CONFIG = {
-  streamUrl: 'https://2kkiendpoint.outoftune.dev/yume.mp3',
-  statusUrl: 'https://2kkiendpoint.outoftune.dev/status-json.xsl',
-  mount: '/yume.mp3',
+  streamUrl: 'http://217.154.126.21:8000/live.flac',
+  statusUrl: 'http://217.154.126.21:8080/nowplaying',   // <-- new endpoint
   recentTitlesLimit: 12,
   metadataPollMs: 10000
 };
